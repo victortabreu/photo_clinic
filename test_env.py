e6bc981447860067d198b43251421bb872e30cf8
victortabreu <victortabreu@gmail.com>
2025-12-29 20:29:50 -0300

    Initial project setup with Python 3.12 environment

---
+++ test_env.py
@@ -0,0 +1,10 @@
+import cv2
+import numpy
+from PIL import Image
+import win32print
+
+print("Python OK")
+print("OpenCV:", cv2.__version__)
+print("NumPy:", numpy.__version__)
+print("Pillow:", Image.__version__)
+print("Win32 OK")
